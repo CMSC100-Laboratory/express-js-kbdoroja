132299667b48d566a3ed9ef665014a82bf8545f4
kbdoroja <kbdoroja@up.edu.ph>
2025-03-12 01:20:32 +0800

    Docs: added documentation to files

--- request.js
+++ request.js
@@ -5,12 +5,12 @@ needle.get("http://localhost:3000/add-book", (err, res) => {
 });
 
 needle.post(
-    "http://localhost:3000/add-book", 
+    "http://localhost:3000/add-book",       //sends this object to be put into books.txt thru post
     { 
-        Bookname: "I hate me", 
-        ISBN: "123-123-213-23", 
-        Author: "ME", 
-        YearPublished: "2005" 
+        Bookname: "This Is How You Lose the Time War", 
+        ISBN: "9781534431003", 
+        Author: "Amal El-Mohtar and Max Gladstone", 
+        YearPublished: "2020" 
     },
     (err, res) => {
         console.log(res.body)
@@ -22,12 +22,12 @@ needle.get("http://localhost:3000/find-by-isbn-author?ISBN=978-0-7475-3269-9&Aut
     console.log(res.body); 
 })
 
-needle.get("http://localhost:3000/find-by-author?Author=J.K+Rowling", (req,res) => {
+needle.get("http://localhost:3000/find-by-author?Author=Me", (req,res) => {
     console.log("Result for Author Search:"); 
-    if(res.body.length == 0){
-        console.log("No matches for that author"); 
+    if(res.body.length == 0){                       //if matchesarray sent is empty, print no matches for the author
+        console.log("No matches for that author");  
     }else{
-        for(let x = 0; x < res.body.length; x++){
+        for(let x = 0; x < res.body.length; x++){   //else prints them one by one
             console.log(res.body[x]); 
         }
     }
--- server.js
+++ server.js
@@ -6,55 +6,53 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({extended: false})); 
 
-var matchesarray = []; 
+var matchesarray = [];  //array for similar author matches
 
-var book = []
+var book = []   //array to put book details
 
 app.get("/add-book", (req,res) =>{
-    res.send("I have received your prodding")
+    res.send("Processing Book Additions")
 })
 
 app.post("/add-book", (req, res) => {
-   book.push(req.body.Bookname);
+   book.push(req.body.Bookname);            //adds the details in post to book array
    book.push(req.body.ISBN); 
    book.push(req.body.Author);
    book.push(req.body.YearPublished); 
 
     if(book.length == 4){
-        if(book[0] && book[1] != 0 && book[2] != 0 && book[3] != 0){
-            appendFileSync("books.txt", book[0]+","+ book[1]+","+ book[2]+","+ book[3] + "\n", "utf-8");
-            book.length = 0; 
-            res.send("success : true");     
+        if(book[0] && book[1] != 0 && book[2] != 0 && book[3] != 0){    //checks if they are non-empty strings
+            appendFileSync("books.txt", book[0]+","+ book[1]+","+ book[2]+","+ book[3] + "\n", "utf-8");    //appends to book.txt
+            book.length = 0;            //resets book array length
+            res.send("success : true");     //sends success message
         }else{res.send("success: false")}
     }else{res.send("success: false")}
    
 })
 
 app.get("/find-by-isbn-author", (req,res) => {
-    var text = readFileSync("books.txt", "utf-8"); 
-    var textarray = text.split("\n"); 
-    for(let x = 0; x < textarray.length; x++){
+    var text = readFileSync("books.txt", "utf-8");  //uses readFileSync to read books.txt
+    var textarray = text.split("\n");               //splits each line into an item in an array
+    for(let x = 0; x < textarray.length; x++){      //checks every array item if it includes query isbn and query author
         if(textarray[x].includes(req.query.ISBN) && textarray[x].includes(req.query.Author)){
-            res.send(textarray[x]); 
+            res.send(textarray[x]);     //as ISBN is unique, sends only one book information
         }
     }
-    res.send("No matches"); 
+    res.send("No matches");         //else send no match
 
 })
 
 app.get("/find-by-author", (req,res) => {
-    matchesarray.length = 0; 
-    var text = readFileSync("books.txt", "utf-8"); 
+    matchesarray.length = 0;                        //resets matches array length
+    var text = readFileSync("books.txt", "utf-8");  
     var textarray = text.split("\n"); 
     for(let x = 0; x < textarray.length; x++){
         if(textarray[x].includes(req.query.Author)){
-            matchesarray.push(textarray[x]); 
+            matchesarray.push(textarray[x]);        //pushes each item in the textarray into the matchesarray for multiple author matches
         }
     }
-    // if(matchesarray.length != 0){
-    //     res.send(matchesarray); 
-    // }else{res.send("No matches"); }
-    res.send(matchesarray); 
+    
+    res.send(matchesarray);     //sends the array whether empty or not
     
     
 })
